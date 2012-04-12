0fe3e5ac8e4ced4d51cf30a0d8a685a2e606337d
Meldanor <Bhaals@gmx.de>
2012-04-12 17:44:37 +0200

    Install and test GraphViz
    Signed-off-by: Meldanor <Bhaals@gmx.de>

--- src/main/java/meldanor/Main.java
+++ src/main/java/meldanor/Main.java
@@ -10,7 +10,12 @@
 
 package meldanor;
 
-import meldanor.training1.Training1;
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+import meldanor.other.GraphViz;
 
 /**
  * @author Meldanor
@@ -19,7 +24,35 @@ import meldanor.training1.Training1;
 public class Main {
 
     public static void main(String[] args) {
-        Training1.test();
+        test();
+    }
+    private static void test() {
+        LinkedList<String> test = new LinkedList<String>();
+        test.add("Kilian");
+        test.add("Gaertner");
+        test.add("Cool");
+
+        // CREATE GRAPH
+        GraphViz gv = new GraphViz();
+        // START GRAPH
+        gv.addln(gv.start_graph());
+
+        // ADD NEW NODES
+        gv.addln(test.get(0) + " -> " + test.get(1) + ";");
+        gv.addln(test.get(1) + " -> " + test.get(2) + ";");
+
+        // END GRAPH
+        gv.addln(gv.end_graph());
+
+        // PRINT OUT STRUCTURE
+        System.out.println(gv.getDotSource());
+
+        // SAVE FILE FROM TYPE PNG
+        String type = "png";
+        File out = new File("out." + type);
+
+        // CREATE IMAGE
+        gv.writeGraphToFile(gv.getGraph(gv.getDotSource(), type), out);
     }
 
 }
