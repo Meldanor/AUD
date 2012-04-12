bbcca1af7a05149efa2e3e3be7df5c6a4967a386
Meldanor <Bhaals@gmx.de>
2012-04-12 23:56:36 +0200

    Implement visualization for double linked lists
    Signed-off-by: Meldanor <Bhaals@gmx.de>

--- src/main/java/meldanor/utils/GraphVizUtils.java
+++ src/main/java/meldanor/utils/GraphVizUtils.java
@@ -19,6 +19,10 @@ import java.util.Iterator;
  */
 public class GraphVizUtils {
 
+    // *******************************
+    // ***** SINGLE LINKED LIST ******
+    // *******************************
+
     /**
      * Create a graph of a single linked list. The output format is PNG and the
      * target file is "out.png"
@@ -79,6 +83,86 @@ public class GraphVizUtils {
             while (iter.hasNext()) {
                 t1 = iter.next();
                 gv.addln(t2.toString() + " -> " + t1.toString() + ";");
+                t2 = t1;
+            }
+        }
+
+        // FINISH GRAPH
+        gv.addln(gv.end_graph());
+
+        // RENAME FILE IF ENDING DIFFER FROM TYPE ENDING
+        if (!target.getName().endsWith(formatType.getType()))
+            target = new File(target.getName().concat(".").concat(formatType.getType()));
+
+        // WRITE FILE
+        gv.writeGraphToFile(gv.getGraph(gv.getDotSource(), formatType.getType()), target);
+    }
+
+    // *******************************
+    // ***** DOUBLE LINKED LIST ******
+    // *******************************
+
+    /**
+     * Create a graph of a double linked list. The output format is PNG and the
+     * target file is "out.png"
+     * 
+     * @param list
+     *            The iterable list
+     */
+    public static void visualizeDoubleLinkedList(Iterable<?> list) {
+        visualizeDoubleLinkedList(list, new File("out.png"), Format.PNG);
+    }
+
+    /**
+     * Create a graph of a double linked list. The output format depends on the
+     * parameter.
+     * 
+     * @param list
+     *            The iterable list
+     * @param targetName
+     *            The name of the file as relative path. If the ending differs
+     *            from the ending of the formatType, the targetName will be
+     *            renamed
+     * @param formatType
+     *            The output file format. See {@link Format}
+     */
+    public static void visualizeDoubleLinkedList(Iterable<?> list, String targetName, Format formatType) {
+        // RENAME FILE IF ENDING DIFFER FROM TYPE ENDING
+        if (!targetName.endsWith(formatType.getType()))
+            targetName = targetName.concat(".").concat(formatType.getType());
+
+        // CREATE GRAPH
+        visualizeDoubleLinkedList(list, new File(targetName), formatType);
+    }
+
+    /**
+     * Create a graph of a double linked list. The output format depends on the
+     * parameter.
+     * 
+     * @param list
+     *            The iterable list
+     * @param target
+     *            The outputFile. If the ending differs from the ending of the
+     *            formatType, the targetName will be renamed
+     * @param formatType
+     *            The output file format. See {@link Format}
+     */
+    public static void visualizeDoubleLinkedList(Iterable<?> list, File target, Format formatType) {
+        // CREATE GRAPH AND HEAD
+        GraphViz gv = new GraphViz();
+        gv.addln(gv.start_graph());
+
+        Iterator<?> iter = list.iterator();
+        // LIST IS NOT EMPTY
+        if (iter.hasNext()) {
+
+            // CREATE NODES OF LIST
+            Object t1 = null;
+            Object t2 = iter.next();
+            while (iter.hasNext()) {
+                t1 = iter.next();
+                gv.addln(t2.toString() + " -> " + t1.toString() + ";");
+                gv.addln(t1.toString() + " -> " + t2.toString() + ";");
                 t2 = t1;
             }
         }
