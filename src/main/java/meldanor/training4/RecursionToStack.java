9defe81fc40da5b76c7ead91303be8a59a9d04e0
Meldanor <Bhaals@gmx.de>
2012-05-13 13:18:04 +0200

    Implement a version wiht a stack
    Signed-off-by: Meldanor <Bhaals@gmx.de>

--- src/main/java/meldanor/training4/RecursionToStack.java
+++ src/main/java/meldanor/training4/RecursionToStack.java
@@ -27,21 +27,37 @@ public class RecursionToStack {
     }
 
     public static int whatStack(int n) {
+        Stack<Integer> stack = new Stack<Integer>();
+        int sum = 0;
+        while (n >= 10) {
+            stack.push(n);
+            n /= 10;
+        }
+        sum = n;
+        while (!stack.is_empty())
+            sum += stack.pop() % 10;
+
+        return sum;
+
+    }
+
+    public static int whatStack2(int n) {
         int sum = 0;
         while (n >= 10) {
             sum += n % 10;
             n /= 10;
         }
+
         return sum + n;
     }
 
     public static void test() {
         Random rand = new Random();
         int j = 0;
+        System.out.println("N\tRec\tStack\tIter");
         for (int i = 0; i < 10; ++i) {
             j = rand.nextInt(100) + 1;
-            System.out.println("N=" + j + "\t" + whatRec(j) + "\t" + whatStack(j));
-
+            System.out.println(j + "\t" + whatRec(j) + "\t" + whatStack(j) + "\t" + whatStack2(j));
         }
 
     }
