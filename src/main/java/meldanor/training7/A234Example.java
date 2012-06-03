07062ff9fdf3e692412d69ba6345a66a4649a1a6
Meldanor <Bhaals@gmx.de>
2012-06-03 17:26:32 +0200

    Add comments
    Signed-off-by: Meldanor <Bhaals@gmx.de>

--- src/main/java/meldanor/training7/A234Example.java
+++ src/main/java/meldanor/training7/A234Example.java
@@ -19,6 +19,18 @@ import aud.util.SingleStepper;
  * 
  */
 public class A234Example extends SingleStepper {
+    /*****************
+     * a) Jeder Knoten hat 2, 3 oder 4 Kindknoten. Jeder Knoten hält jeweils
+     * eine sortierte Liste von 1, 2 oder 3 Keys. Wenn ein Knoten mehr als 3
+     * Keys halten muss, wird ein Split ausgeführt. Das mittlere Element des
+     * aufzusplittenden Knoten wird der Vater Knoten der beiden neuen Knoten.
+     * 
+     * 
+     * 
+     * b) Bei der zweiten Reihenfolge sind nur 2 Splits vonnöten, bei der ersten
+     * Reihenfolge 4 Splits. Auch sind nicht soviele Kindknoten beim 2. nötig
+     * wie beim 1. Algorithmus am Beispiel als Kommentare im Quellcode
+     *********************/
 
     private A234Tree<Integer> tree;
     protected DotViewer v = DotViewer.displayWindow((String) null, "BLA");
@@ -39,15 +51,24 @@ public class A234Example extends SingleStepper {
         A234Example example = new A234Example(tree);
 
         // @formatter:off
-
+        // In Wurzel
         tree.insert(5);     example.halt("5");
+        // in Wurzel, vor 5
         tree.insert(1);     example.halt("1");
+        // in Wurzel, nach 5
         tree.insert(19);    example.halt("19");
+        // Split -> 5 Wird wurzel, 1 linker teilbaum, 19 und 25 rechter
         tree.insert(25);    example.halt("25");
+        // bei 19 und 25 einfügen
         tree.insert(17);    example.halt("17");
+        // Split bei 17, 19, 21 und 25 -> 19 in die wurzel ziehen, 17 wird 2. teilbaum  von wurzel und 21 und 25 rechter
         tree.insert(21);    example.halt("21");
+        // 9 zwischen 5 und 19 und mittlerer knoten existiert -> bei 17 einfügen
         tree.insert(9);     example.halt("9");
-        tree.insert(15);    example.halt("15");
+        // 15 zwischen 5 und 19 und mittlerer knoten existiert -> bei 9,17 einfügen
+        tree.insert(15);    example.halt("15");  
+        // 14 zwischen 5 und 19 und mittlerer knoten existiert -> bei 9,15,17 einfügen
+        // SPLIT -> 9,14,15,17. 9 wird zweiter Kindknoten der Wurzel, 14 in die Wurzel und 15 und 17 bleiben
         tree.insert(14);    example.halt("14");
 
         example.halt("QUIT");
@@ -59,15 +80,26 @@ public class A234Example extends SingleStepper {
 
         // @formatter:off
 
-        tree2.insert(25);   example2.halt("5");
-        tree2.insert(9);    example2.halt("1");
-        tree2.insert(14);   example2.halt("19");
-        tree2.insert(1);    example2.halt("25");
+        // 25 in Wurzel
+        tree2.insert(25);   example2.halt("25");
+        // 9 in Wurzel, sortieren
+        tree2.insert(9);    example2.halt("9");
+        // 14 in Wurzel, sortieren
+        tree2.insert(14);   example2.halt("14");
+        // 1 in Wurzel -> Split bei 1,9,14,25
+        // 9 wird wurzel , 1 ist linker teilbaum, 14 und 25 der rechte knoten
+        tree2.insert(1);    example2.halt("1");
+        // 17 größer 9 -> bei 14 und 25 einfügen
         tree2.insert(17);   example2.halt("17");
+        // 21 größer 9 -> bei 14, 17 und 25 einfügen -> Split bei 14, 17, 21 , 25
+        // 17 in die Wurzel, 14 wird 2. linker kindknoten von wurzel und 21 und 25 bleiben
         tree2.insert(21);   example2.halt("21");
-        tree2.insert(15);   example2.halt("9");
-        tree2.insert(5);    example2.halt("15");
-        tree2.insert(19);   example2.halt("14");
+        // 15 zwischen 9 und 17 -> in 2. linken kindknoten einfügen (14)
+        tree2.insert(15);   example2.halt("15");
+        // 5 kleiner 9 -> bei 1. linken kindknoten einfügen (1)
+        tree2.insert(5);    example2.halt("5");
+        // 19 größer 17 -> in 3. linken kindknoten einfügen
+        tree2.insert(19);   example2.halt("19");
 
         example2.halt("QUIT");
 
--- src/main/java/meldanor/training7/AVLExample.java
+++ src/main/java/meldanor/training7/AVLExample.java
@@ -20,6 +20,20 @@ import aud.util.SingleStepper;
  */
 public class AVLExample extends SingleStepper {
 
+    /*****************
+     * a) Ein AVL Baum ist ein binärer Baum, dessen Höhe vom linken und rechten
+     * Teilbaum sich höchstens um 1 unterscheiden. Es werden binäre Bäume
+     * ausgeglichen, da sonst Bäume nach vielen Einfüge- und Entfernoperationen
+     * zu einer Liste degenerieren können. Der Aufwand für eine Suche in so
+     * einem Baum verkommt dann von O(log n) zu O(n). Um diesen entgegen zu
+     * kommen, wird bei einem AVL Baum der folgende Algorithmus angewandt:
+     * 
+     * b) Der Knoten wird gemäß dem Algorithmus eines binären Suchbaumes
+     * eingefügt. Danach wird überprüft, ob die AVL Eigenschaft verletzt wird.
+     * Wenn eine Verletzung vorliegt, wird dies per Rotation der Teilbäume
+     * wiederhergestellt. Beschreibung am Beispiel im Quellcode
+     *********************/
+
     private AVLTree<Integer, Integer> tree;
     protected DotViewer v = DotViewer.displayWindow((String) null, "BLA");
 
@@ -39,13 +53,20 @@ public class AVLExample extends SingleStepper {
         AVLExample example = new AVLExample(tree);
 
         // @formatter:off
-
+        // Keine Verletzung
         tree.insert(14, null);    example.halt("14");
+        // Keine Verletzung
         tree.insert(15, null);    example.halt("15");
+        // Verletzung des Kriteriums - Rechter Teilbaum größer. Einmal Links rotieren
         tree.insert(17, null);    example.halt("17");
+        // Keine Verletzung
         tree.insert(7,  null);    example.halt("7");
+        // Verletzung des Kriteriums - Linker Teilbaum größer. Einmal rechts rotieren(von der sieben aus)
         tree.insert(5,  null);    example.halt("5");
+        // Verletzung des Kriteriums - Linker Teilbaum größer. Zweimal links rotieren(von der vierzehn aus)
+        // Dann Rechts rotieren
         tree.insert(10, null);    example.halt("10");
+        // Verletzung des Kriteriums - Rechter Teilbaum von 15 größer. Einmal rechts rotieren und dann einmal links
         tree.insert(16, null);    example.halt("16");
         example.halt("QUIT");
 
