d0517b16eeb6e47eac0fd812975498838a0876bb
Meldanor <Bhaals@gmx.de>
2012-07-08 15:55:44 +0200

    Solve first task with prim
    Signed-off-by: Meldanor <Bhaals@gmx.de>

---
+++ src/main/java/meldanor/training12/PrimAlgorithm.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 2012 Kilian Gaertner
+ * 
+ * Dieser Quelltext ist Open Source und kann von jedem verwendet werden, der 
+ * folgende Bedingung einhält:
+ * Jeder, der den Quelltext, ob in Teilen oder komplett,nutzt, muss dem Inhabenden
+ * des Copyrights eine Pizza spendieren, sollte derjenige dem Inhabenden des Copyrights
+ * begegnen.
+ */
+
+package meldanor.training12;
+
+import java.util.ArrayList;
+
+import aud.example.graph.DijkstraShortestPaths;
+import aud.example.graph.MyEdge;
+import aud.example.graph.MyGraph;
+import aud.example.graph.MyNode;
+import aud.example.graph.PrimMinimumSpanningTree;
+import aud.example.graph.TraversalExample;
+
+/**
+ * @author Meldanor
+ * 
+ */
+public class PrimAlgorithm {
+
+    public static void main(String[] args) {
+        MyGraph graph = new MyGraph(false);
+        ArrayList<MyNode> nodes = new ArrayList<MyNode>();
+
+        for (int i = 0; i < 6; ++i)
+            nodes.add(graph.addNode());
+
+        ArrayList<MyEdge> edges = new ArrayList<MyEdge>();
+        MyEdge e; // temp
+
+        // LINK NODES AND WEIGHT THEM
+        // 0 to 1
+        e = graph.addEdge(nodes.get(0), nodes.get(1));
+        e.setWeight(2);
+        edges.add(e);
+        // 0 to 2
+        e = graph.addEdge(nodes.get(0), nodes.get(2));
+        e.setWeight(5);
+        edges.add(e);
+        // 0 to 3
+        e = graph.addEdge(nodes.get(0), nodes.get(3));
+        e.setWeight(3);
+        edges.add(e);
+
+        // 1 to 5
+        e = graph.addEdge(nodes.get(1), nodes.get(5));
+        e.setWeight(4);
+        edges.add(e);
+
+        // 2 to 3
+        e = graph.addEdge(nodes.get(2), nodes.get(3));
+        e.setWeight(1);
+        edges.add(e);
+        // 2 to 4
+        e = graph.addEdge(nodes.get(2), nodes.get(4));
+        e.setWeight(7);
+        edges.add(e);
+        // 2 to 5
+        e = graph.addEdge(nodes.get(2), nodes.get(5));
+        e.setWeight(8);
+        edges.add(e);
+
+        // 3 to 5
+        e = graph.addEdge(nodes.get(3), nodes.get(5));
+        e.setWeight(6);
+        edges.add(e);
+
+        // 4 to 5
+        e = graph.addEdge(nodes.get(4), nodes.get(5));
+        e.setWeight(9);
+        edges.add(e);
+
+        // visit 0 [p=null, d=0.0]
+        // visit 1 [p=0, d=2.0]
+        // visit 3 [p=0, d=3.0]
+        // visit 2 [p=3, d=1.0]
+        // visit 5 [p=1, d=4.0]
+        // visit 4 [p=2, d=7.0]
+
+        // VISUALIZE
+        TraversalExample ts = new TraversalExample();
+
+        PrimMinimumSpanningTree ds = new PrimMinimumSpanningTree(graph);
+        ts.traversal = ds;
+
+        ds.singlestepper = ts;
+        ds.start(nodes.get(0));
+
+    }
+
+}
