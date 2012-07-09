25389c4ad9660a4006eb24ec1b8676215253e2a8
Meldanor <Bhaals@gmx.de>
2012-07-09 15:45:26 +0200

    Add benchmark for the binom problem
    Signed-off-by: Meldanor <Bhaals@gmx.de>

--- src/main/java/meldanor/training12/MyBinom.java
+++ src/main/java/meldanor/training12/MyBinom.java
@@ -10,6 +10,9 @@
 
 package meldanor.training12;
 
+import java.text.DecimalFormat;
+import java.util.Random;
+
 /**
  * @author Meldanor
  * 
@@ -59,6 +62,16 @@ public class MyBinom {
         return results[n][k];
     }
 
+    public static int binom2(int n, int k) {
+        if (k == 0 || n == k)
+            return 1;
+        if (k == 1 || n == k + 1)
+            return n;
+        else
+            return binom2(n - 1, k) + binom2(n - 1, k - 1);
+    }
+
+    // NOT USED
     private static void printMatrix(int[][] a) {
         if (a == null) {
             System.out.println("Matrix is null!");
@@ -73,9 +86,65 @@ public class MyBinom {
         System.out.println();
     }
 
-    public static void main(String[] args) {
-        System.out.println(binom(7, 5));
-        printMatrix(results);
-    }
+    private static final int TEST_COUNTER = 200;
+    private static final int MAX_A = 20;
+    private static final int MAX_B = 20;
 
+    public static void main(String[] args) {
+
+        // GENERATE TEST NUMBERS
+
+        int[][] testPairs = new int[TEST_COUNTER][2];
+        Random rand = new Random();
+
+        for (int i = 0; i < TEST_COUNTER; ++i) {
+            testPairs[i][0] = rand.nextInt(MAX_A);
+            testPairs[i][1] = rand.nextInt(MAX_B);
+            // k must be lower than n
+            if (testPairs[i][0] < testPairs[i][1])
+                --i;
+        }
+
+        // MEASURE VALUES
+
+        long t1 = System.currentTimeMillis();
+        long t2 = System.currentTimeMillis();
+
+        long total1 = 0L;
+        long total2 = 0L;
+
+        // TEMP VAR FOR BETTER READING
+        int a = 0;
+        int b = 0;
+
+        for (int i = 0; i < TEST_COUNTER; ++i) {
+            a = testPairs[i][0];
+            b = testPairs[i][1];
+
+            // DYNAMIC
+            t1 = System.nanoTime();
+            System.out.println("Dynamic:\ta=" + a + "\tb=" + b + "\tResult = " + binom(a, b));
+            total1 += System.nanoTime() - t1;
+
+            // NON DYNAMIC
+            t2 = System.nanoTime();
+            System.out.println("Non-Dynamic:\ta=" + a + "\tb=" + b + "\tResult = " + binom2(a, b));
+            total2 += System.nanoTime() - t2;
+
+            System.out.println();
+        }
+
+        // PRINT OUT RESULTS
+        total1 /= TEST_COUNTER;
+        total2 /= TEST_COUNTER;
+
+        double percent1 = (double) total1 / (double) total2 * 100.0;
+        double percent2 = (double) total2 / (double) total1 * 100.0;
+
+        DecimalFormat f = new DecimalFormat("#0.00");
+
+        System.out.println("Dynamic:\t" + total1 + " ns\t(" + f.format(percent1) + "% of Non-Dynamic)");
+        System.out.println("Non-Dynamic:\t" + total2 + " ns\t(" + f.format(percent2) + "% of Dynamic)");
+
+    }
 }
